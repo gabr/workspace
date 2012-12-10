3b785091d251f1f0da4fbf7a33d3005cae9dafdb
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2012-12-10 23:52:01 +0100

    first

---
+++ HelloWorld/gen/com/example/gabr/helloworld/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.gabr.helloworld;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ PierwszaAplikacja/gen/com/example/gabr/pierwszaaplikacja/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.gabr.pierwszaaplikacja;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
