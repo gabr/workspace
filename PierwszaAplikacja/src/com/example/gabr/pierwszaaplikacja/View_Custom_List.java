91b1c3b7dd3c189c2d515166e6ca8fdffee92458
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2012-12-11 09:44:29 +0100

    moja lista

--- PierwszaAplikacja/src/com/example/gabr/pierwszaaplikacja/View_Custom_List.java
+++ PierwszaAplikacja/src/com/example/gabr/pierwszaaplikacja/View_Custom_List.java
@@ -12,6 +12,13 @@ public class View_Custom_List extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		ListView rozbudowana_lista = (ListView) findViewById(R.id.listView1);
+		String[] dane = {"aaaaa",
+						 "bbbbb",
+						 "ccccc",
+						 "ddddd"};
+		List_Custom_List adapter_listy = new List_Custom_List(this, dane);
+		rozbudowana_lista.setAdapter(adapter_listy);
+		
 	}
 
 }
