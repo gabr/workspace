8d06b1c0bcc25afeb7b75a5483dc121e5c94b55b
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2012-12-26 12:00:17 +0100

    porzadki, obym nic nie zniszczyl

--- Lekcja 5 - funkcje/src/Zmienne.java
+++
@@ -1,9 +0,0 @@
-public class Zmienne {
-	public static void main(String args[]) {
-		double promienKola = 10.0;
-		double obwodKola = promienKola*2*Math.PI;
-		
-		System.out.println("Obwód koła o promieniu: "+promienKola+" wynosi:" +
-				Math.round(obwodKola*100)/100.0);
-	}
-}
